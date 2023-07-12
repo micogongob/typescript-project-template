ba979e28b3d24c11c70b4988b1ea9b9223769d41
micogongob <15572400+micogongob@users.noreply.github.com>
2023-07-12 15:09:15 +0800

    Remove custom blacklist validation

--- modular/libs/starter-web/src/starter/validation.starter.ts
+++ modular/libs/starter-web/src/starter/validation.starter.ts
@@ -1,6 +1,5 @@
 import { ValidatorFunction } from '../types';
 import { HttpBasedException } from '../errors';
-import { STANDARD_BLACKLIST_CHAR_REGEX } from '@local/starter-core';
 
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
@@ -8,53 +7,15 @@ import { z } from 'zod';
 export class HeaderValidationStarter {
   static build(validator: z.ZodTypeAny): ValidatorFunction {
     return (req: Request, res: Response, next: NextFunction): Response | void => {
-      validateRequestHeaders(req, validator);
-      return next();
-    };
-  }
-}
-
-export class HeaderBlackListValidationStarter {
-  static build(
-    validator: z.ZodTypeAny,
-    validateHeaders: string[],
-    pattern: RegExp = STANDARD_BLACKLIST_CHAR_REGEX
-  ): ValidatorFunction {
-    return (req: Request, res: Response, next: NextFunction): Response | void => {
-      validateRequestHeaders(req, validator);
-
-      for (const header of validateHeaders) {
-        console.log(header);
-        const toValidate: string | string[] | undefined = req.headers[header];
-
-        if (toValidate !== undefined) {
-          if (typeof toValidate === 'string') {
-            validateStringRegex(`${header} header`, toValidate, pattern);
-          } else {
-            for (const subHeaders of toValidate) {
-              validateStringRegex(`${header} header`, subHeaders, pattern);
-            }
-          }
-        }
+      const zParse = validator.safeParse(req.headers);
+      if (!zParse.success) {
+        console.error(JSON.stringify(zParse.error.errors));
+        // TODO pass to exception e.g for string
+        // TODO setup .refine or superRefine for black list validation
+        // [{"code":"invalid_type","expected":"string","received":"undefined","path":["rrn"],"message":"Required"}]
+        throw HttpBasedException.badRequest();
       }
-
       return next();
     };
-  }
-}
-
-function validateRequestHeaders(req: Request, validator: z.ZodTypeAny): void {
-  const zParse = validator.safeParse(req.headers);
-  if (!zParse.success) {
-    console.error(JSON.stringify(zParse.error.errors));
-    // TODO pass to exception e.g for string
-    // [{"code":"invalid_type","expected":"string","received":"undefined","path":["rrn"],"message":"Required"}]
-    throw HttpBasedException.badRequest();
-  }
-}
-
-function validateStringRegex(fieldName: string, aString: string, pattern: RegExp): void {
-  if (pattern.test(aString)) {
-    throw HttpBasedException.badRequest(`${fieldName} has invalid characters.`);
   }
 }

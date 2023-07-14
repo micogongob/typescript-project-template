e5dcdfb98a78aba6bf450d30c18693ab89ff0ddd
micogongob <15572400+micogongob@users.noreply.github.com>
2023-07-14 12:38:14 +0800

    Collection all http request validation in one middleware

--- modular/libs/starter-web/src/starter/validation.starter.ts
+++ modular/libs/starter-web/src/starter/validation.starter.ts
@@ -5,51 +5,73 @@ import { ErrorParser } from '@local/starter-core';
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
 
-// TODO refactor to hold only in one middleware for validation
-export class HeaderValidationStarter {
-  static build(validator: z.ZodTypeAny): ValidatorFunction {
+type RequestPathParameters = 'PATH_PARAMETERS';
+type RequestHeaders = 'HEADERS';
+type RequestBody = 'BODY';
+type RequestType = RequestPathParameters | RequestHeaders | RequestBody;
+
+export type ValidationSpecStarter = {
+  pathParameters?: z.ZodTypeAny;
+  headers?: z.ZodTypeAny;
+  body?: z.ZodTypeAny;
+};
+
+export class HttpRequestValidationStarter {
+  static build(spec: ValidationSpecStarter): ValidatorFunction {
     return (req: Request, res: Response, next: NextFunction): Response | void => {
-      const zParse = validator.safeParse(req.headers);
-      if (!zParse.success) {
-        throw HttpBasedException.badRequest(
-          'Request header validation failed',
-          ...ErrorParser.zodErrorToStrings(zParse.error)
-        );
+      const errors: string[] = [];
+
+      if (spec.pathParameters !== undefined) {
+        this.validate('PATH_PARAMETERS', req, spec.pathParameters, errors);
       }
-      req['requestHeaders'] = zParse.data;
-      return next();
+      if (spec.headers !== undefined) {
+        this.validate('HEADERS', req, spec.headers, errors);
+      }
+      if (spec.body !== undefined) {
+        this.validate('BODY', req, spec.body, errors);
+      }
+
+      if (errors.length > 0) {
+        throw HttpBasedException.badRequest(...errors);
+      } else {
+        // TODO add wrapper function for router middelware by default has try catch block
+        return next();
+      }
     };
   }
-}
 
-export class BodyValidationStarter {
-  static build(validator: z.ZodTypeAny): ValidatorFunction {
-    return (req: Request, res: Response, next: NextFunction): Response | void => {
-      const zParse = validator.safeParse(req.body);
-      if (!zParse.success) {
-        throw HttpBasedException.badRequest(
-          'Request body validation failed',
-          ...ErrorParser.zodErrorToStrings(zParse.error)
-        );
-      }
-      req['requestBody'] = zParse.data;
-      return next();
-    };
-  }
-}
-
-export class PathValidationStarter {
-  static build(validator: z.ZodTypeAny): ValidatorFunction {
-    return (req: Request, res: Response, next: NextFunction): Response | void => {
-      const zParse = validator.safeParse(req.params);
-      if (!zParse.success) {
-        throw HttpBasedException.badRequest(
-          'Path validation failed',
-          ...ErrorParser.zodErrorToStrings(zParse.error)
-        );
-      }
-      req['requestPath'] = zParse.data;
-      return next();
-    };
+  private static validate(type: RequestType, req: Request, validator: z.ZodTypeAny, errors: string[]): void {
+    let paramsToValidate: unknown;
+    let requestField: string;
+    let additionalErrorMessage: string;
+  
+    switch (type) {
+      case 'PATH_PARAMETERS':
+        paramsToValidate = req.params;
+        requestField = 'requestPathParameters';
+        additionalErrorMessage = 'Path parameter validation failed.';
+        break;
+      case 'HEADERS':
+        paramsToValidate = req.headers;
+        requestField = 'requestHeaders';
+        additionalErrorMessage = 'Request headers validation failed.';
+        break;
+      case 'BODY':
+        paramsToValidate = req.body;
+        requestField = 'requestBody';
+        additionalErrorMessage = 'Request body validation failed.';
+        break;
+      default:
+        throw new Error('Unknown request type provided');
+    }
+  
+    const validated = validator.safeParse(paramsToValidate);
+  
+    if (validated.success) {
+      req[requestField] = validated.data;
+    } else {
+      errors.push(additionalErrorMessage);
+      errors.push(...ErrorParser.zodErrorToStrings(validated.error));
+    }
   }
 }

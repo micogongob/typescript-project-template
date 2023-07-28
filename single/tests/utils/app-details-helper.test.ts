6941798df80f01395ff465577c1a233c8a993839
micogongob <15572400+micogongob@users.noreply.github.com>
2023-07-28 12:09:53 +0800

    Refactor mocha test into jest test for single

---
+++ single/tests/utils/app-details-helper.test.ts
@@ -0,0 +1,15 @@
+// import { expect } from 'chai';
+import { AppDetailsHelper } from '../../src/utils';
+
+describe('App Details Helper', () => {
+  describe('getDetails', () => {
+    test('should return unknown as default', () => {
+      // when
+      const details = AppDetailsHelper.getDetails();
+
+      // then
+      expect(details.name).toBe('typescript-project-template');
+      expect(details.version).toBe('1.0.0');
+    });
+  });
+});
